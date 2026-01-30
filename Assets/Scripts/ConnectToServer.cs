8cbc5894149f56c839a811e2d9c143dc3515babb
ItIsMrDanish <danishbassboostbusiness@gmail.com>
2026-01-30 14:34:34 +0100

    Added Loading and Lobby scene with Photon setup

---
+++ Assets/Scripts/ConnectToServer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+
+public class ConnectToServer : MonoBehaviourPunCallbacks
+{
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to Master Server");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("Joined Lobby");
+        SceneManager.LoadScene("MainMenu");
+    }
+}
