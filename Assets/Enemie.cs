c0f69d84a8970133e41431b3ede52d705ce5f380
Mathi-ass <mathias.f93@yahoo.com>
2026-02-15 14:28:10 +0100

    Enemie update
    Enemie is now chasing the player, and following the nav mesh

--- Assets/Enemie.cs
+++ Assets/Enemie.cs
@@ -1,16 +1,30 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemie : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Transform player;
+    private NavMeshAgent agent;
+
     void Start()
     {
-        
+        // Find the player by tag
+        GameObject playerObj = GameObject.FindGameObjectWithTag("Player");
+        if (playerObj != null)
+        {
+            player = playerObj.transform;
+        }
+
+        // Get the NavMeshAgent component
+        agent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (player != null && agent != null)
+        {
+            // Set the agent's destination to the player's position
+            agent.SetDestination(player.position);
+        }
     }
 }
